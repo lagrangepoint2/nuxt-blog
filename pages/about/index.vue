0ecfd23cfaab18ee1acc5b35fc0ddba66f924aef
Thongvun Nak <tnak@Administrators-MacBook-Pro.local>
2019-04-23 15:06:19 -0600

    first commit uploading finished blog

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo minima quibusdam commodi, vitae suscipit dolorum deleniti obcaecati? Voluptatum exercitationem totam aliquid necessitatibus! Numquam cupiditate id voluptatem similique facilis maxime cum.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo minima quibusdam commodi, vitae suscipit dolorum deleniti obcaecati? Voluptatum exercitationem totam aliquid necessitatibus! Numquam cupiditate id voluptatem similique facilis maxime cum.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo minima quibusdam commodi, vitae suscipit dolorum deleniti obcaecati? Voluptatum exercitationem totam aliquid necessitatibus! Numquam cupiditate id voluptatem similique facilis maxime cum.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo minima quibusdam commodi, vitae suscipit dolorum deleniti obcaecati? Voluptatum exercitationem totam aliquid necessitatibus! Numquam cupiditate id voluptatem similique facilis maxime cum.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo minima quibusdam commodi, vitae suscipit dolorum deleniti obcaecati? Voluptatum exercitationem totam aliquid necessitatibus! Numquam cupiditate id voluptatem similique facilis maxime cum.</p>
+    </div>
+</template>
+
+<style scoped>
+.about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
+
